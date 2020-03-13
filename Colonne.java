319a3a3de7eaae6788a4ecb588fe5e8c33c28fa1
Guillaume Huet <huet41u@etu.univ-lorraine.fr>
2020-03-13 09:21:15 +0100

    Methode de base

--- Colonne.java
+++ Colonne.java
@@ -2,4 +2,11 @@ import java.util.ArrayList;
 
 class Colonne {
   private ArrayList<Jeton> jetons;
+
+
+
+
+  public Jeton getJeton(int i){
+    return this.jetons.get(i);
+  }
 }
--- Jeton.java
+++ Jeton.java
@@ -1,3 +1,21 @@
 class Jeton {
-  
+  private int id;
+
+  public Jeton(int joueur){
+    try {
+      this.id = joueur;
+    }catch(Exception e){
+      e.printStackTrace();
+      System.out.println("Ce n'est pas un nombre valide");
+    }
+  }
+
+  public int getId(){
+    return this.id;
+  }
+
+  @Override
+  public String toString(){
+    return ""+this.id;
+  }
 }
