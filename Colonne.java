45fa27fe07411b7acff90522e6db0e0610989795
Thomas Bouillon <tbouillon54@gmail.com>
2020-03-13 09:22:38 +0100

    Affichage de la grille

--- Colonne.java
+++ Colonne.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 
 class Colonne {
   private ArrayList<Jeton> jetons;
+
+  public Colonne(int nb_lignes) {
+    jetons = new ArrayList(nb_lignes);
+  }
 }
--- Grille.java
+++ Grille.java
@@ -1,13 +1,38 @@
 class Grille {
-  private Colonne colonnes;
+  private Colonne colonnes[];
+  private int nb_lignes;
+
+  public Grille(int nb_lignes, nb_colonnes) {
+    colonnes = new Colonne[nb_colonnes];
+    for(int i=0; i<nb_colonnes; i++) {
+      colonnes[i] = new Colonne(nb_lignes);
+    }
+    this.nb_lignes=nb_lignes;
+  }
 
   public void afficher() {
-    for (int i=0; i<10; i++) {
-      for (int j=0; j<10; j++) {
-        System.out.print("x ");
+    afficherSepLigne();
+    Jeton jeton;
+    String jeton_str;
+    for(int i=0; i<nb_colonnes; i++) {
+      for(int j=0; j<nb_lignes; j++) {
+        jeton = colonnes[i].getJeton();
+        if(jeton == null)
+          jeton_str = " ";
+        else
+          jeton_str = jeton.toString();
+        System.out.print("|"+jeton_str);
       }
-      System.out.println();
+      System.out.println("|");
+      afficherSepLigne();
     }
+  }
+
+  public void afficherSepLigne() {
+    for(int i=0; i < colonnes.length; i++) {
+      System.out.print("+-");
+    }
+    System.out.println("+");;
   }
 
   public Grille Trier(){
